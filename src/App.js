0c8c634011ffbc3e410f31f66339e85f83510aed
Shefaa <shefaasomar@gmail.com>
2021-07-05 09:04:43 +0300

    Added form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
+import { Form } from './components/Form';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello React! 👋</h1>
+      <header>
+        <h1>Shefaa's TODO</h1>
+      </header>
+      <Form
+        setInput={setInput}
+        tasks={tasks}
+        input={input}
+        setTasks={setTasks} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export const Form = ({ setInput, setTasks, input, tasks }) => {
+    const handleTask = (event) => {
+        console.log(event);
+        const text = event.target.value;
+        setInput(text);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTasks(
+            (tasks) => {
+                return [...tasks, {text: input, completed: false}];
+            }
+        )
+        setInput("");
+        console.log(tasks);
+    }
+
+    return (
+        <form>
+            <input
+                value={input}
+                onChange={handleTask}
+                type="text"
+                className="todo-input" />
+            <button
+                onClick={handleSubmit}
+                type="submit"
+                className="todo-button" >
+                    <i className="fas fa-plus-square" />
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all" >All</option>
+                    <option value="completed" >Completed</option>
+                    <option value="uncompleted" >Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+}
